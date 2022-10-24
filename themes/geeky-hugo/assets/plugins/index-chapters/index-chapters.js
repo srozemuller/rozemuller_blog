f56efa2b88e5f2560d204cbb5c2ea51819fe0a50
Sander Rozemuller <srozemuller@uniccloud.nl>
2022-10-24 15:53:27 +0200

    code update

--- themes/geeky-hugo/assets/plugins/index-chapters/index-chapters.js
+++ themes/geeky-hugo/assets/plugins/index-chapters/index-chapters.js
@@ -19,4 +19,4 @@ var adsElements = document.getElementsByClassName("ads");
 console.log(adsElements);
 $(".ads").append('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8130597253774789" crossorigin="anonymous"></script>')
 $(".ads").append('<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8130597253774789" data-ad-slot="6791833895"></ins>')
-$(".ads").append('<script> (adsbygoogle = window.adsbygoogle || []).push({});</script>')
+$(".ads").append('<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>')
